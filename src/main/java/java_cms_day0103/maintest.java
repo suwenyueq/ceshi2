1fd89fb43fa324dca345c4867681bb966b387152
suwenyueq <1569751721@qq.com>
2020-01-03 08:11:35 +0800

    测试2

---
+++ src/main/java/java_cms_day0103/maintest.java
@@ -0,0 +1,12 @@
+package java_cms_day0103;
+
+/**
+ * @author 苏文越
+ *
+ * 2020年1月3日
+ */
+public class maintest {
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
+}
